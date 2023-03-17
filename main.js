3015702631c7b6eb939ddff3058233877ea51c4b
Mateusz <m.wesolowski.92@gmail.com>
2023-03-17 23:33:58 +0100

    refactoring render functions

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-const productSection = document.querySelector(".products");
-
 const searchInput = document.querySelector(".search-bar-input");
 
 const basketBtn = document.querySelector(".basket-clear-btn");
@@ -10,37 +8,6 @@ const currentProducts = products;
 
 let basketBtns;
 let basket = [];
-
-const renderProduct = function (products) {
-	productSection.innerHTML = "";
-	products.forEach((item) => {
-		const element = document.createElement("div");
-		element.innerHTML = `<div class="product-item ${
-			item.sale ? "on-sale" : ""
-		}">
-<img src="${item.image}" alt="Phone">
-<p class="product-name">${item.name}</p>
-<p class="product-description">${item.description}
-</p>
-<div class="product-price">
-  <span class="price">${item.price.toFixed(2)}</span>
-  <span class="price-sale">${(item.saleAmount
-		? item.price - item.saleAmount
-		: item.price
-	).toFixed(2)}</span>
-</div>
-<button data-id="${
-			item.id
-		}" class="product-add-to-basket-btn">Dodaj do koszyka</button>
-<p class="product-item-sale-info">Promocja</p>
-</div>`;
-		productSection.appendChild(element);
-	});
-
-	/// pobieramy tutaj, bo inaczej po zmianie kategorii nie działaja porzyciski
-	basketBtns = document.querySelectorAll(".product-add-to-basket-btn");
-	basketBtns.forEach((btn) => btn.addEventListener("click", addToBasket));
-};
 
 const renderCategories = function () {
 	const categories = currentProducts
@@ -55,24 +22,26 @@ const renderCategories = function () {
 
 	const htmlCategories = document.querySelector(".categories-items");
 
-	allCategories.forEach((item, index) => {
+	allCategories.forEach((item) => {
 		const newCategory = document.createElement("button");
 		newCategory.dataset.key = item;
-		// index === 0 ? newCategory.classList.add('active') : '';
 		newCategory.textContent = item;
 		htmlCategories.appendChild(newCategory);
 	});
-	// ewentulanie dostać się za pomocą selektora first-child
-	document.querySelector(".categories-items button").classList.add("active");
+	document
+		.querySelector(".categories-items button:first-child")
+		.classList.add("active");
 };
 
 renderCategories();
+
+basketBtns = document.querySelector(".product-item");
+console.log(basketBtns);
 
 const categoriesButtons = document.querySelectorAll(".categories-items button");
 
 const filterCategory = function () {
 	const category = this.dataset.key.toLowerCase();
-
 	const categoryItems = currentProducts.filter((item) => {
 		if (item.category === category) return item;
 	});
@@ -93,14 +62,12 @@ categoriesButtons.forEach((button) =>
 
 searchInput.addEventListener("input", (e) => {
 	const text = e.target.value.toLowerCase();
-	let elemets;
-	elemets = currentProducts.filter((element) =>
+	let elemets = currentProducts.filter((element) =>
 		element.category.toLowerCase().includes(text)
 	);
 	if (elemets.length === 0) {
 		document.querySelector(".empty-state").style.display = "block";
-	}
-	else {
+	} else {
 		document.querySelector(".empty-state").style.display = "none";
 	}
 	renderProduct(elemets);
@@ -128,5 +95,38 @@ const clearBasket = () => {
 };
 
 basketBtn.addEventListener("click", clearBasket);
+
+const renderProductHtml = function (item) {
+	const productCard = document.createElement("div");
+	productCard.innerHTML = `<div class="product-item ${
+		item.sale ? "on-sale" : ""
+	}">
+<img src="${item.image}" alt="Phone">
+<p class="product-name">${item.name}</p>
+<p class="product-description">${item.description}
+</p>
+<div class="product-price">
+<span class="price">${item.price.toFixed(2)}</span>
+<span class="price-sale">${(item.saleAmount
+		? item.price - item.saleAmount
+		: item.price
+	).toFixed(2)}</span>
+</div>
+<button data-id="${
+		item.id
+	}" class="product-add-to-basket-btn">Dodaj do koszyka</button>
+<p class="product-item-sale-info">Promocja</p>
+</div>`;
+	return productCard;
+};
+
+const renderProduct = function (products) {
+	const productSection = document.querySelector(".products");
+	productSection.innerHTML = "";
+	const renderedProducts = products.forEach((product) => {
+		productSection.appendChild(renderProductHtml(product));
+	});
+	return renderedProducts;
+};
 
 renderProduct(currentProducts);
